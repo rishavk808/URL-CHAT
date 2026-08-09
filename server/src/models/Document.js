226607f63724d2e73d2cb043dc052d7efcbed7d5
Rishav Kumar <rishavk808@gmail.com>
2026-08-10 00:50:08 +0530

    Document model done

--- server/src/models/Document.js
+++ server/src/models/Document.js
@@ -1,1 +1,26 @@
 import mongoose from 'mongoose';
+
+const DocumentSchema = new mongoose.Schema({
+  url: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true
+  },
+  title: {
+    type: String,
+    required: true,
+    default: 'Untitled Webpage'
+  },
+  chunkCount: {
+    type: Number,
+    required: true,
+    default: 0
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+export default mongoose.model('Document', DocumentSchema);
